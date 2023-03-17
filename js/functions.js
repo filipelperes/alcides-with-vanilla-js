598c899801e1cb70ce1f63d7fa6b5954c1dd9d00
Filipe Lago Peres <filipelperes@gmail.com>
2023-03-17 09:26:01 -0300

    start

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,5 @@
 (($) => {
     $(() => {
-        const swiperProps = {
-            loop: true,
-            setWrapperSize: true,
-            direction: 'vertical',
-            spaceBetween: 36,
-            touchEventsTarget: 'container',
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-            //autoHeight: true,
-            freemode: true,
-
-            navigation: {
-                enable: false,
-            },
-
-            breakpoints: {
-                // when window width is >= 768px
-                768: {
-                    freemode: false,
-                    slidesPerView: 3,
-                    slidesPerGroup: 3,
-                    direction: 'horizontal',
-                    touchEventsTarget: 'wrapper',
-                    navigation: {
-                        enable: true,
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    },
-                },
-            },
-        }
-        const swiper = new Swiper('.swiper-container', swiperProps);
         const btnProduto = $('.button-produto');
         const btnPhone = $('a.phone');
 
@@ -67,7 +35,7 @@
 
         //MOSTRAR EM QUAL PAGINA ESTÁ
         let url = window.location.href.split('/');
-        $(`nav.menu a[href=${ url[url.length - 1] === "" ? 'home' : url[url.length - 1] }]`).addClass('menu-selected');
+        $(`.menu-logo a[href=${ url[url.length - 1] !== "" ? url[url.length - 1] : 'home'  }]`).addClass('menu-selected');
 
         const slideToggle = (el) => {
             $(el).slideToggle();
